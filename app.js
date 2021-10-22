60ac0d88e9b708b54af1d30e90364c85a6628888
GinKalp <80583818+GinKalp@users.noreply.github.com>
2021-10-22 09:45:19 +0300

    added title

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+// JSX
+const result = 5*43
+
+const App =
+    <div className={'container'}>
+        <h1 className={"kazkas"}>Hello, world!</h1>
+        <h2>Other title 5 * 43 = {result} </h2>
+    </div>
+
+ReactDOM.render(App, document.getElementById('root'));
+
+const title = (
+    <div className={'container'}>
+        <h1>THE BAND</h1>
+            <h5><i> We love music</i></h5>
+    </div>
+)
+ReactDOM.render(title, document.getElementById('title'));
